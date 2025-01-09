415e9280ee391e7c942ade1874147b899dd24b5f
Temitope Afolabi <tafolabi@renmoney.com>
2025-01-09 15:30:06 +0100

    initial commit

---
+++ salary_logic.py
@@ -0,0 +1,13 @@
+from variables import *
+
+def salary_decision(salary,customer_type,color):
+    if salary < 5000:
+        decision = decision_decline
+    elif customer_type == "Self Employed" and salary < 100000:
+        decision = decision_decline
+    elif salary < 50000 and color == "Black":
+        decision = decision_decline
+    else:
+        decision = decision_approve
+    
+    return decision
