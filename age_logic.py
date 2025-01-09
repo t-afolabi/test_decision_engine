b12347c5dcead9c9d9e7ae6f7948069c7ac08ac6
Temitope Afolabi <tafolabi@renmoney.com>
2025-01-09 17:54:50 +0100

    commit

--- age_logic.py
+++ age_logic.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from variables import *
 
 
@@ -12,18 +11,3 @@ def age_decision(age, gender):
 
     return decision
 
-=======
-from variables import *
-
-
-def age_decision(age, gender):
-    if age > 60:
-        decision = decision_decline
-    elif age < 18 and gender == "Male":
-        decision = decision_decline
-    else:
-        decision = decision_approve
-
-    return decision
-
->>>>>>> 37acecf453e6c893182e304f3b3aebaaa9f01ac5
--- salary_logic.py
+++ salary_logic.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from variables import *
 
 def salary_decision(salary,customer_type,color):
@@ -12,18 +11,3 @@ def salary_decision(salary,customer_type,color):
         decision = decision_approve
     
     return decision 
-=======
-from variables import *
-
-def salary_decision(salary,customer_type,color):
-    if salary < 5000:
-        decision = decision_decline
-    elif customer_type == "Self Employed" and salary < 100000:
-        decision = decision_decline
-    elif salary < 50000 and color == "Black":
-        decision = decision_decline
-    else:
-        decision = decision_approve
-    
-    return decision
->>>>>>> 37acecf453e6c893182e304f3b3aebaaa9f01ac5
